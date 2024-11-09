3fae3f812c330be778c9b60826d4b80b6080ee46
João Pedro <joaopedron253@gmail.com>
2024-11-09 12:08:45 -0300

    first commit: initial structure

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Teachers from './pages/Teachers'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/teachers' element={<Teachers />} />
+        <Route path='/teachers/:speciality' element={<Teachers />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:teachId' element={<Appointment />} />
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
